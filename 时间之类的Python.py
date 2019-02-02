321997f6968c61059a7bf26d02d3a1d80a3900aa
zhangxiaofeng9971zxf <zhangxiaofeng9971@126.com>
2019-02-03 07:15:55 +0800

    python 中 内置time的常见用法

---
+++ 时间之类的Python.py
@@ -0,0 +1,27 @@
+#获取当前时间
+# from datetime import datetime
+# now = datetime.now()#获取计算机当前时间
+# print(now)
+
+# from datetime import datetime
+# dt = datetime(2015,4,19,12) #指定时间
+# print(dt)
+#
+# from datetime import datetime
+# now = datetime.now()
+# timestamp = now.timestamp()#把1970-0101为0,现在的时间是多少?
+# print(timestamp)
+#
+# from datetime import datetime
+# t = 1429417200.0
+# print(datetime.fromtimestamp(t))#计算机时间跟实际时间相互转换
+# print(datetime.utcfromtimestamp(t))#格林威治时间
+#
+# from datetime import datetime
+# cday = datetime.strptime('2015-07-2','%Y-%m-%d')#把字符串的时间 格式改为数字类型的
+# print(cday)
+
+# from  datetime import datetime
+# now = datetime.now()
+# print(now.strftime('%Y-%m-%d %H:%M:%S'))#改为字符串格式的
+
